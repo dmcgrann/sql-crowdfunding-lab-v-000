329f44c940744598a147dca046784d7cce1b32e6
David McGrann <dpmcgrann@gmail.com>
2018-07-06 14:42:35 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,20 +1,20 @@
-CREATE TABLE projects 
-  (id INTEGER PRIMARY KEY,
-    title TEXT,
-    funding_goal INTEGER,
-    start_date DATE,
-    end_date DATE
+CREATE TABLE projects ( 
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date DATE
 );
 
-CREATE TABLE users
-  (id INTEGER PRIMARY KEY,
-    name TEXT,
-    age INTEGER
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
 );
 
-CREATE TABLE pledges
-  (id INTEGER PRIMARY KEY,
-    amount INTEGER,
-    user_id INTEGER,
-    project_id INTEGER,
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER,
 );
