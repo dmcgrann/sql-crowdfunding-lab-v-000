c17b94ce0615cc1595e39848abc6c0f820c9159f
David McGrann <dpmcgrann@gmail.com>
2018-07-06 14:37:49 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -7,4 +7,11 @@ CREATE TABLE projects (
 );
 
 CREATE TABLE users (
-  )
+  id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+    amount INTEGER)
