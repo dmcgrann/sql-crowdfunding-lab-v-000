6436b7d590ec3d6bc224cccbee4c88e40db9ffaa
David McGrann <dpmcgrann@gmail.com>
2018-07-06 15:57:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,8 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT projects.title FROM projects
     JOIN pledges ON pledges.id = projects.id
-    GROUP BY projects.title, pledges.amount" 
+    GROUP BY pledges.amount
+    ORDER BY projects.title ASC" 
    
 end
 
